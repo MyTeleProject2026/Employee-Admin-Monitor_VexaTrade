b566c674650ec40aacde57e413bb13935023d478
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-06-22 13:29:07 -0400

    Update App.jsx

--- frontend-EmployeeMonitor/src/App.jsx
+++ frontend-EmployeeMonitor/src/App.jsx
@@ -1,19 +1,31 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import UsersList from './pages/UsersList';
 import UserDetail from './pages/UserDetail';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users" element={<UsersList />} />
-        <Route path="/users/:userId" element={<UserDetail />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/*"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/users" element={<UsersList />} />
+                <Route path="/users/:userId" element={<UserDetail />} />
+              </Routes>
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
