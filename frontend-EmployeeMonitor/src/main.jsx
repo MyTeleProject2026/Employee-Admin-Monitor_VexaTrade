a7bcdd7eea7ec3c444b0f001897df23ba1e77ee4
CryptoPulse <267598483+MyTeleProject2026@users.noreply.github.com>
2026-06-23 01:48:41 -0400

    Update main.jsx

--- frontend-EmployeeMonitor/src/main.jsx
+++ frontend-EmployeeMonitor/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
-import ErrorBoundary from './components/ErrorBoundary'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </BrowserRouter>
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
